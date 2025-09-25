3889f902be08000bfd5742a88d9f5598bca47954
unknown <bigjolemaire@gmail.com>
2025-09-25 18:59:02 +0000

    core: created checkLogin, getUserID, checkUserRole

--- settings/core.php
+++ settings/core.php
@@ -7,17 +7,24 @@ session_start();
 ob_start();
 
 //funtion to check for login
-if (!isset($_SESSION['id'])) {
-    header("Location: ../Login/login_register.php");
-    exit;
+function checkLogin() {
+    if (!isset($_SESSION['id'])) {
+        header("Location: ../Login/login_register.php");
+        exit;
+    }   
 }
 
-
 //function to get user ID
-
+function getUserID() {
+    if (isset($_SESSION['id'])) return true; 
+    else return false;
+}
 
 //function to check for role (admin, customer, etc)
-
+function checkUserRole() {
+    if ($_SESSION['role'] == 2) return true; 
+    else if ($_SESSION['role']) return false;
+}
 
 
 ?>
