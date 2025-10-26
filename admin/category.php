5acc6372e204e990c82ddc1dd0ffaee516cf1caf
unknown <bigjolemaire@gmail.com>
2025-10-26 15:35:52 +0000

    category: created basic initial skeleton

---
+++ admin/category.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- NB: recipe-feed.html is in pages folder -->
+	<title> Admin Dashboard </title>
+	<meta name = 'viewport' content = 'width = device-width, initial-scale = 1.0'>
+</head>
+ 
+<body>
+	<h1> Admin Dashboard </h1>
+    <button> Retrieve </button>
+    <button> Create </button>
+    <button> Update </button>
+    <button> Delete </button>
+
+    <div id = 'create-form'></div>
+</body>
+</html>
