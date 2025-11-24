54725cd230ea3d2a375c9e27781f3981cd8263ca
unknown <bigjolemaire@gmail.com>
2025-11-24 16:22:24 +0000

    db-conn: made supabase nonexportable

--- settings/db-conn.js
+++ settings/db-conn.js
@@ -3,24 +3,24 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://gizyyugpvcqvlbjoaotd.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey) // Supabase client 'connect-point'
+const supabase = createClient(supabaseUrl, supabaseKey) // Supabase client 'connect-point'
 
 // insert data to Supabase db
-export async function insertDataToSupabase(supaObj, tableName, ...fieldData) {
+export async function insertDataToSupabase(tableName, ...fieldData) {
     // testerId, levelId, duration, pourNumber
     const dataToInsert = {}; // initialising object with data to be added to respective fields in db table
     fieldData.forEach(field => dataToInsert[field[0]] = field[1]) // adding data to be added to db to dataTotInsert; index 0 of field in loop is field name, 1 is field datum
     console.log(dataToInsert)
 
-    const { error } = await supaObj
+    const { error } = await supabase
         .from(tableName)
         .insert(dataToInsert);
     console.log(error);
 }
 
 // Fetch data from Supabase db
-export async function fetchDataFromSupabase(supaObj, tableName, fieldName, fieldValue) {
-    const { data, error } = await supaObj // checking for value
+export async function fetchDataFromSupabase(tableName, fieldName, fieldValue) {
+    const { data, error } = await supabase // checking for value
         .from(tableName)
         .select()
         .eq(fieldName, fieldValue);
